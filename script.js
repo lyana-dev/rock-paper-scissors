22d1404e25f6c705037d5d74358d3e622760dffc
Lyana <lyana.amir23@gmail.com>
2025-11-27 23:27:52 +0000

    Update script.js to include arrays
    The game is altered to run 5 times, so that the following occurs:
    
    Store the user guesses and return them to the user when they finish or win.
    
    Bonus: computer guesses are also returned.

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 // initialise variables with the default values (constructors)
 let gameRound = 0; //setting a new game
-const maxRound = 3;
+const maxRound = 5;
 let stopGame = false;
+let userChoices = []; //array to store user's choices
+let computerChoices = [] //array for computer's choices
 
 function play(userChoice) {
     /* The return statement ends function execution and specifies a value to be returned to the function caller.
@@ -12,7 +14,7 @@ function play(userChoice) {
     } 
 
     if (gameRound >= maxRound) {
-        document.getElementById("result").innerHTML = "Game over! You have played 3 rounds...";
+        document.getElementById("result").innerHTML = "Game over! You have played 5 rounds...";
         hide();
         return;
     }
@@ -23,6 +25,10 @@ function play(userChoice) {
     let choice = ["Rock", "Paper", "Scissors"];
     var computerChoice = choice[Math.floor(Math.random() * 3)];
     let displayResults = "";
+
+    // storing the user's & computer's guesses in the array 
+    userChoices.push(userChoice);
+    computerChoices.push(computerChoice);
 
     // checking for a tie
     if (userChoice == computerChoice) {
@@ -62,8 +68,9 @@ function play(userChoice) {
         }
     }
 
-    if (gameRound >= maxRound) {
-        displayResults = displayResults + "<br><br>Game over!<br>You played 3 rounds.";
+    if (gameRound >= maxRound || stopGame == true) {
+        displayResults = displayResults + "<br><br>Game over!<br>You played " +gameRound +" rounds.";
+        displayResults = displayResults + "<br><br>Your choices: " + userChoices.toString() + "<br>Computer's choices: " + computerChoices.toString();
         hide();
     }
 
